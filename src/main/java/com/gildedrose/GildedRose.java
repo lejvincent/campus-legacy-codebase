ce9ea77b58010922a0697723d612f115cafa5cda
lejvincent <lej.vincent26@gmail.com>
2019-01-07 15:54:40 +0100

    GildedRose Class Updated

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 public class GildedRose {
     Item[] items;
+    org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(GildedRose.class);
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,14 +10,38 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            logger.debug("START ->" + " Name = " + items[i].name + ", SellIn = " + items[i].sellIn + ", Quality = " + items[i].quality);
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                logger.debug("Name is not 'Aged Brie' and not 'Backstage passes to a TAFKAL80ETC concert'");
                 if (items[i].quality > 0) {
+                    logger.debug("Quality is more than 0");
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        logger.debug("Name is not 'Sulfuras, Hand of Ragnaros'");
+                        if (items[i].name.contains("Conjured")) {
+                            logger.debug("Name contains 'Conjured'");
+                            if (items[i].quality == 1) {
+                                logger.debug("Item contains 'Conjured' and its quality is equals to 1. Actually = " + items[i].quality);
+                                items[i].quality = items[i].quality - 1;
+                                logger.debug("And quality lost 1 decrease to " + items[i].quality);
+                            } else if (items[i].sellIn < 0) {
+                                logger.debug("Item contains 'Conjured' and its quality is under to 0. Actually = " + items[i].quality);
+                                items[i].quality = items[i].quality - 2;
+                                logger.debug("And quality lost 2 and decrease to " + items[i].quality);
+                            } else {
+                                logger.debug("Item contains 'Conjured' and its quality is above to 1. Actually = " + items[i].quality);
+                                items[i].quality = items[i].quality - 1;
+                                logger.debug("And quality lost 1 decrease to " + items[i].quality);
+                            }
+                        } else {
+                            logger.debug("Name don't contains 'Conjured'");
+                            items[i].quality = items[i].quality - 1;
+                            logger.debug("And quality lost 1 and decrease to " + items[i].quality);
+                        }
                     }
                 }
             } else {
+                logger.debug("Is 'Aged Brie' or 'Backstage passes to a TAFKAL80ETC concert'");
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -57,6 +82,7 @@ public class GildedRose {
                     }
                 }
             }
+            logger.debug("END ->" + " Name = " + items[i].name + ", SellIn = " + items[i].sellIn + ", Quality = " + items[i].quality);
         }
     }
 
