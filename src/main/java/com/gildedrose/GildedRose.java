960c312e4d06eb9c7c398b80f17bb88f742446bd
lejvincent <lej.vincent26@gmail.com>
2019-01-08 16:57:09 +0100

    GildedRose.java Updated

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -39,13 +39,17 @@ public class GildedRose {
         if (item.quality < 50 && item.sellIn >= 0) {
             item.quality = item.quality + 1;
         } else if (item.quality > 0 && item.sellIn < 0) {
-            if (item.quality == 1) {
-                item.quality = item.quality - 1;
-            } else {
-                item.quality = item.quality - 2;
-            }
+            Anno(item, 1, item.quality - 1, item.quality - 2);
         }
         return item;
+    }
+
+    private void Anno(Item item, int i, int i2, int i3) {
+        if (item.quality == i) {
+            item.quality = i2;
+        } else {
+            item.quality = i3;
+        }
     }
 
     public Item isAgedBrie(Item item) {
@@ -62,24 +66,24 @@ public class GildedRose {
         reports = reports + "is TAFKAL80ETC Concert";
         item.sellIn = item.sellIn - 1;
 
+        CM(item);
+        return item;
+    }
+
+    private void CM(Item item) {
         if (item.quality < 50 && item.sellIn < 11 && item.sellIn > 5) {
-            if (item.quality == 49) {
-                item.quality = item.quality + 1;
-            } else {
-                item.quality = item.quality + 2;
-            }
+            CM1(item, 2);
         } else if (item.quality < 50 && item.sellIn < 6 && item.sellIn > 0) {
-            if (item.quality == 49) {
-                item.quality = item.quality + 1;
-            } else {
-                item.quality = item.quality + 3;
-            }
+            CM1(item, 3);
         } else if (item.quality < 50 && item.sellIn == 0) {
             item.quality = 0;
         } else if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
-        return item;
+    }
+
+    private void CM1(Item item, int i) {
+        Anno(item, 49, item.quality + 1, item.quality + i);
     }
 
     public Item isConjured(Item item) {
