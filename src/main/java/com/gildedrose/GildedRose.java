2cf211781989c620a54ca7cdc6cd6fe9d739fd92
lejvincent <lej.vincent26@gmail.com>
2019-01-08 16:59:47 +0100

    GildedRose.java Updated

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,7 +25,7 @@ public class GildedRose {
             }
             reports = reports + "END -> Name = " + item.name + ", SellIn = " + item.sellIn + ", Quality = " + item.quality;
         }
-        logger.debug("Reports : " + reports);
+        logger.debug("Reports : <br>" + reports);
     }
 
     public Item[] getItems() {
@@ -39,17 +39,13 @@ public class GildedRose {
         if (item.quality < 50 && item.sellIn >= 0) {
             item.quality = item.quality + 1;
         } else if (item.quality > 0 && item.sellIn < 0) {
-            Anno(item, 1, item.quality - 1, item.quality - 2);
+            if (item.quality == 1) {
+                item.quality = item.quality - 1;
+            } else {
+                item.quality = item.quality - 2;
+            }
         }
         return item;
-    }
-
-    private void Anno(Item item, int i, int i2, int i3) {
-        if (item.quality == i) {
-            item.quality = i2;
-        } else {
-            item.quality = i3;
-        }
     }
 
     public Item isAgedBrie(Item item) {
@@ -66,31 +62,31 @@ public class GildedRose {
         reports = reports + "is TAFKAL80ETC Concert";
         item.sellIn = item.sellIn - 1;
 
-        CM(item);
-        return item;
-    }
-
-    private void CM(Item item) {
         if (item.quality < 50 && item.sellIn < 11 && item.sellIn > 5) {
-            CM1(item, 2);
+            if (item.quality == 49) {
+                item.quality = item.quality + 1;
+            } else {
+                item.quality = item.quality + 2;
+            }
         } else if (item.quality < 50 && item.sellIn < 6 && item.sellIn > 0) {
-            CM1(item, 3);
+            if (item.quality == 49) {
+                item.quality = item.quality + 1;
+            } else {
+                item.quality = item.quality + 3;
+            }
         } else if (item.quality < 50 && item.sellIn == 0) {
             item.quality = 0;
         } else if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
-    }
-
-    private void CM1(Item item, int i) {
-        Anno(item, 49, item.quality + 1, item.quality + i);
+        return item;
     }
 
     public Item isConjured(Item item) {
         reports = reports + "is Conjured";
         item.sellIn = item.sellIn - 1;
 
-        if (item.quality == 1) {
+        if (item.quality > 0 && item.quality == 1) {
             item.quality = item.quality - 1;
         } else if (item.quality > 0 && item.sellIn < 0) {
             item.quality = item.quality - 2;
